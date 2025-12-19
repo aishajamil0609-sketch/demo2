c88d036b3f2dc94a2af6ae239384069a133bffed
aishajamil0609-sketch <aisha.jamil0609@gmail.com>
2025-12-19 00:36:07 -0800

    Add files via upload

---
+++ q-1 (a).cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<< "The most likely way for the world to be destroyed , \n\t Most experts agree, 'is by accident'.\n\t\t That's where we come in; \n\"We're computer professionals. We cause accidents.\"";
+}
